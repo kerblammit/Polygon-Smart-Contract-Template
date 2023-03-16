a202e427ff3df38d8481bac27113a00389b2aedf
HashbrownKazang <125426777+kerblammit@users.noreply.github.com>
2023-03-16 18:32:12 +0000

    Initial commit

---
+++ contract (copy).sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract SimpleStorage {
+  uint256 storedData;
+	
+  function get() public view returns (uint) {
+    return storedData;
+  }
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function double() public {
+    storedData *= 2;
+  }
+}
+
+contract MathTest {
+	function add(uint a, uint b) public pure returns (uint) {
+		return a+b;
+	}
+	
+	function multiply(uint a, uint b) public pure returns (uint) {
+	    return a*b;
+	}
+}
